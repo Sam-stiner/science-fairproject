7ce1214110fc954adc05e225c0411caa10095563
Sam-stiner <sam.stiner@outlook.com>
2014-03-30 18:27:21 -0700

    Update sam.py

--- sam.py
+++ sam.py
@@ -4,27 +4,33 @@ from Tkinter import Entry
 import tkMessageBox
 
 top = Tkinter.Tk()
-global mass
+
+global earthmass
 global vilosity
 global distance
 global time
 
+global massbox
+global vilositybox
+global distancebox
+global timebox
+
 def earth():
-    label1=Label(top, text="what is the mass of the satilite (kg)").grid(row=2)
-    mass=Entry(top, bd=5)
-    mass.grid(row=2, column=2, columnspan=3)
+    label1 = Label (top, text="what is the mass of the satilite (kg)").grid(row=2)
+    massbox = Entry (top, bd=5)
+    massbox.grid (row=2, column=2, columnspan=3)
     
-    label2=Label(top, text="what is the vilosity of the satilite (m/s)").grid(row=3)
-    vilosity=Entry(top, bd =5)
-    vilosity.grid(row=3, column=2, columnspan=3)
+    label2=Label (top, text="what is the vilosity of the satilite (m/s)").grid(row=3)
+    vilositybox = Entry (top, bd =5)
+    vilositybox.grid (row=3, column=2, columnspan=3)
     
-    label3=Label(top, text="how far is the satilte from earth (km)").grid(row=4)
-    distance=Entry(top, bd=5)
-    distance.grid(row=4, column=2, columnspan=3)
+    label3 = Label (top, text="how far is the satilte from earth (km)").grid (row=4)
+    distancebox = Entry (top, bd=5)
+    distancebox.grid (row=4, column=2, columnspan=3)
     
-    label4=Label(top, text="how long has the satilite been in orbit").grid(row=5)
-    time=Entry(top, bd=5)
-    time.grid(row=5, column=2, columnspan=3)
+    label4 = Label (top, text="how long has the satilite been in orbit").grid(row=5)
+    timebox = Entry (top, bd=5)
+    timebox.grid (row=5, column=2, columnspan=3)
     
     o = Tkinter.Button (top, text="next", command  = earth1).grid(row=5, column=7)
 
@@ -33,29 +39,22 @@ def earth1():
     vilosity = vilositybox.get
     distance = distancebox.get
     time = timebox.get
+    
     if mass.isdigit():
-        mass=int(mass)
+        mass = int(mass)
     if vilosity.isdigit():
-        vilosity=int(vilosity)
+        vilosity = int(vilosity)
     if distance.isdigit():
-        distance=int(distance)
+        distance = int(distance)
     if time.isdigit():
-        time=int(time)
+        time = int(time)
    
-    f=(m*v**2)/r
-    a1=Label(top, text =  'the net cintripital force is').grid(row=7)
+    radius = distance+695500 
+    force = (mass*vilosity**2)/radius
+    a1 = Label(top, text =  'the net cintripital force is').grid(row = 7)
     print f
 
-
-    
-
-    
-    
-
-   
-a = Tkinter.Button (top, text = "obgect in orbit around earth", command = earth).grid(row=1, column=1)
-
-
+a = Tkinter.Button (top, text = "obgect in orbit around earth", command = earth).grid (row = 1, column = 1)
 
 top.mainloop()
 
